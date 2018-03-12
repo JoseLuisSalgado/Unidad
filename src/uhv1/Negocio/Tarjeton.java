53aa8cc7d6af511817182eecb34391dba68a305e
evil5 <evil5@192.168.0.23>
2018-03-12 16:42:15 -0600

    Creada clase Tarjeton

--- src/uhv1/Negocio/Tarjeton.java
+++ src/uhv1/Negocio/Tarjeton.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package uhv1.Negocio;
 
-/**
- *
- * @author evil5
- */
 public class Tarjeton {
     
 }
