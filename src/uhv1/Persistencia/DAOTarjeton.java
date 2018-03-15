33d0d2f2995bda806e6ff8cc4f5e69f8f04b9e29
evil5 <evil5@192.168.0.23>
2018-03-15 13:21:04 -0600

    Creado DAOTajeton con método insertaTarjeton

--- src/uhv1/Persistencia/DAOTarjeton.java
+++ src/uhv1/Persistencia/DAOTarjeton.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package uhv1.Persistencia;
 
-/**
- *
- * @author darky
- */
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.Calendar;
+import uhv1.Negocio.Casa;
+import uhv1.Negocio.Responsable;
+
 public class DAOTarjeton {
     
+    public void insertaTarjeton(Responsable hab, String plac) throws SQLException{
+        java.sql.Date fechaActual; //calcula la fecha actual
+        fechaActual = java.sql.Date.valueOf(LocalDate.now());
+        Calendar cal = Calendar.getInstance();//intsancia para sumar un mes a la fecha actual
+        cal.add(Calendar.MONTH, 1);//suma un mes
+        java.sql.Date fechaVencimiento = new java.sql.Date(cal.getTimeInMillis());//cast para Date de sql
+        System.out.println(fechaActual);
+        System.out.println(fechaVencimiento);
+        try {
+            String insertaSQL = "Insert INTO tarjeton(Habitantes_idHabitante, placas, fecha_impresion, fecha_vencimiento, estado) VALUES ("+hab.getId()+", '"+ plac +"', '" +fechaActual+"', '" +fechaVencimiento+"', 'Activo')";
+            Statement statement = ManejadorBD.dameConnection().createStatement();
+            statement.execute(insertaSQL);
+            System.out.println("Se ha agregado Tarjetón correctamente");
+        
+        } catch (SQLException e) {
+           e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) throws SQLException {
+        Casa cas = new Casa(1, "D", 105);
+        String placas = "123ACDC";
+        Responsable hab = new Responsable(1, "Eleazar", "Morales", "Garduño", 12312412, cas, 20);
+        DAOTarjeton dao = new DAOTarjeton();
+        dao.insertaTarjeton(hab, placas);
+    }
 }
