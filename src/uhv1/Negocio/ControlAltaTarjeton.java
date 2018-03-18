dd6c065f0fecbfcd9b0bad1af6bf61c7a110e40b
evil5 <evil5@192.168.0.23>
2018-03-17 18:23:06 -0600

    Cambio a algunos nombres de métodos

--- src/uhv1/Negocio/ControlAltaTarjeton.java
+++ src/uhv1/Negocio/ControlAltaTarjeton.java
@@ -9,8 +9,9 @@ public class ControlAltaTarjeton {
     public ControlAltaTarjeton(){//constuctor default
     }
     
-    public ControlAltaTarjeton(Responsable hab){
-        ControlVentanaAltaTarjeton cvat = new ControlVentanaAltaTarjeton(hab);
+    public void inicia(Responsable hab){
+        ControlVentanaAltaTarjeton cvat = new ControlVentanaAltaTarjeton();
+        cvat.creaVentanaAltaTarjeton(hab);
     }
     
     public void enviaDAOAltaTajeton(Responsable hab, String plac) throws SQLException{
@@ -25,7 +26,8 @@ public class ControlAltaTarjeton {
     
     public static void main(String[] args) {
         Casa cas = new Casa(1, "D", 105);
-        Responsable res = new Responsable(1, "Eleazar", "Morales", "Gadrduño", 1225623, cas, 140);
-        ControlAltaTarjeton cat = new ControlAltaTarjeton(res);
+        Responsable res = new Responsable(1, "Eleazar", "Morales", "Gadrduño", 1225623, cas, 50);
+        ControlAltaTarjeton cat = new ControlAltaTarjeton();
+        cat.inicia(res);
     }
 }
