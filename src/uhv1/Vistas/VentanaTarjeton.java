53775e69a953b8c857e748f7dc27eaeccfe990a2
evil5 <evil5@192.168.0.23>
2018-03-18 23:40:36 -0600

    Botón de Alta referencia a ControlAltaTarjetón y manda responsable

--- src/uhv1/Vistas/VentanaTarjeton.java
+++ src/uhv1/Vistas/VentanaTarjeton.java
@@ -5,6 +5,7 @@
  */
 package uhv1.Vistas;
 
+import uhv1.Negocio.ControlAltaTarjeton;
 import uhv1.Negocio.ControlGestionTarjeton;
 import uhv1.Negocio.Responsable;
 import uhv1.Negocio.Tarjeton;
@@ -58,6 +59,11 @@ public class VentanaTarjeton extends javax.swing.JFrame {
         jLabel1.setText("Ventana Tarjeton");
 
         jButton1.setText("Alta ");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButtonBaja.setText("Baja");
         jButtonBaja.addActionListener(new java.awt.event.ActionListener() {
@@ -131,6 +137,12 @@ public class VentanaTarjeton extends javax.swing.JFrame {
     private void formAncestorMoved(java.awt.event.HierarchyEvent evt) {//GEN-FIRST:event_formAncestorMoved
         // TODO add your handling code here:
     }//GEN-LAST:event_formAncestorMoved
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        ControlAltaTarjeton cat = new ControlAltaTarjeton();
+        cat.inicia(res);
+        dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
  
     // Variables declaration - do not modify//GEN-BEGIN:variables
